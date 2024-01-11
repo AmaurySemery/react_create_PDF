2240a505ded51e423d2fc1a0c3e7e09c58f6ad53
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-12 00:48:11 +0100

    Afficher conditionnellement un component enfant

--- reacstimate/src/App.js
+++ reacstimate/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Info from './components/info';
+import EstimateForm from './components/EstimateForm';
 
 class App extends Component {
   state = {
@@ -9,12 +10,20 @@ class App extends Component {
 
   handleShowEstimateForm = evt => {
     console.log(evt);
+    this.setState({
+      showForm: !this.state.showForm
+    })
   };
 
   render() { 
     return (
-      <Info onHandleShowEstimateForm={this.handleShowEstimateForm} />
+      <div>
+        <Info onHandleShowEstimateForm={this.handleShowEstimateForm} />
+        <br/>
+        {this.state.showForm && <EstimateForm />}
+      </div>
     );
+    // Si c'est true, on affiche le formulaire et si c'est false, on n'attendra jamais la partie "et" donc on n'affichera pas le formulaire
   }
 }
  

557236fad2983e04006a0fbb586bc693a8690c43
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-11 20:39:14 +0100

    Passer des props à un component enfant

---
+++ reacstimate/src/components/info.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    render() { 
+        return (
+            <div>
+        <h2>Bienvenue sur votre générateur de devis</h2>
+        <div>
+          Créer un devis ? C'est simple : cliquez sur {" "}
+          <button onClick={this.props.onHandleShowEstimateForm} >nouveau devis</button>
+        </div>
+      </div>
+        );
+    }
+}
+ 
+export default Info;

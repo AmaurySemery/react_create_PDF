3b2da5b973dc1d657e017fdefdc0433c2c3e8e12
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-12 12:38:11 +0100

    Stateless functional component

--- reacstimate/src/components/info.jsx
+++ reacstimate/src/components/info.jsx
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Info extends Component {
-    render() { 
-        return (
-            <div>
-        <h2>Bienvenue sur votre générateur de devis</h2>
-        <div>
-          Créer un devis ? C'est simple : cliquez sur {" "}
-          <button onClick={this.props.onHandleShowEstimateForm} >nouveau devis</button>
-        </div>
+// class Info extends Component {
+//     render() { 
+//         return (
+//         <div>
+//           <h2>Bienvenue sur votre générateur de devis</h2>
+//           <div>
+//             Créer un devis ? C'est simple : cliquez sur {" "}
+//             <button onClick={this.props.onHandleShowEstimateForm} >nouveau devis</button>
+//           </div>
+//       </div>
+//         );
+//     }
+// }
+
+// Reçoit des props et ça l'aide à construire du contenu graphique avec stateless functional component (component bête)
+// Ce component n'a plus d'état, il a juste des props
+// C'est un type de component plus simple
+
+const Info = (props) => {
+  return ( 
+    <div>
+          <h2>Bienvenue sur votre générateur de devis</h2>
+          <div>
+            Créer un devis ? C'est simple : cliquez sur {" "}
+            <button onClick={props.onHandleShowEstimateForm} >nouveau devis</button>
+          </div>
       </div>
-        );
-    }
+   );
 }
- 
+  
 export default Info;

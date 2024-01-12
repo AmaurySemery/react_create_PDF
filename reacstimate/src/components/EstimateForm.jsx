e39f8a1062f17524e424493e2d801cc38075c860
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-12 12:54:22 +0100

    Faire d'un champ texte un controlled component

--- reacstimate/src/components/EstimateForm.jsx
+++ reacstimate/src/components/EstimateForm.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class EstimateForm extends Component {
-    state = {  } 
+    state = { 
+        estimateTitle: ''
+     };
 
     handleSubmit = evt => {
         evt.preventDefault();
         console.log('généré');
+    }
+
+    handleChange = evt => {
+        const title = evt.currentTarget.value;
+        console.log(title);
+        this.setState({
+            estimateTitle: title
+        })
     }
 
     render() { 
@@ -13,7 +23,7 @@ class EstimateForm extends Component {
             <React.Fragment>
             <div>Nouveau devis</div>
             <form onSubmit={this.handleSubmit}>
-                <input type="text" name="estimate-title" id='estimate-title' placeholder='titre du devis' />
+                <input type="text" name="estimate-title" id='estimate-title' placeholder='titre du devis' value={this.state.estimateTitle} onChange={evt => this.handleChange(evt)} />
                 <button type='submit' >Générer le devis</button>
             </form>
             </React.Fragment>

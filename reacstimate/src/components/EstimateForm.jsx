6ab4e8c7bc6432bd71544898c10d631d8d3e91ae
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-12 12:43:51 +0100

    Création d'un formulaire

--- reacstimate/src/components/EstimateForm.jsx
+++ reacstimate/src/components/EstimateForm.jsx
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class EstimateForm extends Component {
     state = {  } 
+
+    handleSubmit = evt => {
+        evt.preventDefault();
+        console.log('généré');
+    }
+
     render() { 
         return (
-            <h2>Formulaire</h2>
+            <React.Fragment>
+            <div>Nouveau devis</div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="estimate-title" id='estimate-title' placeholder='titre du devis' />
+                <button type='submit' >Générer le devis</button>
+            </form>
+            </React.Fragment>
         );
     }
 }
